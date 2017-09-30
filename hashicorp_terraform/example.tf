32abd376931246fd4e7960c4b4b223b290120ff9
dennyzhang <denny@dennyzhang.com>
2017-09-29 21:26:28 -0700

    update doc

--- hashicorp_terraform/example.tf
+++ hashicorp_terraform/example.tf
@@ -5,10 +5,11 @@ provider "digitalocean" {
   token = "${var.do_token}"
 }
 
-# Create a new Web Droplet in the nyc2 region
-resource "digitalocean_droplet" "web" {
+resource "digitalocean_droplet" "elasticsearch" {
   image  = "ubuntu-14-04-x64"
-  name   = "denny-vm1-1"
+  name   = "denny-es-test1"
   region = "sfo2"
   size   = "512mb"
+  user_data = "#cloud-config\nruncmd:\n  - touch /tmp/a.txt"
+  ssh_keys = [1968722,979830,812123]
 }
